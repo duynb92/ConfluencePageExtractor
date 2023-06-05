978a8b368a1fb9e164d6e1dee498b4a529f1a18a
Duy <duynb92@gmail.com>
2023-06-05 23:14:47 +0700

    Update fetch_site_id trigger

--- triggers/fetch_site_id.js
+++ triggers/fetch_site_id.js
@@ -1,12 +1,41 @@
+const siteOutputFields = async (z, bundle) => {
+  z.console.log("here");
+   const response = await z.request({
+    url: 'https://api.atlassian.com/oauth/token/accessible-resources',
+    method: 'GET',
+    headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${bundle.authData.access_token}`,
+    },
+    params: {}
+  })
+    .then(response => {
+      let json = response.json;
+      z.console.log(json);
+      let result = json.map(x=> {
+        let isJira = x.scopes.find(x => x.includes('jira')) != null;
+        var id = x.id;
+        if (isJira) {
+          id = `${id} - jira`
+        } else {
+          id = `${id} - confluence`
+        }
+
+        return {
+          id: id,
+          url: x.url
+        }
+      });
+      z.console.log(result);
+      return result;
+    })
+    return response;
+  };
+
+
 module.exports = {
   operation: {
-    perform: {
-      headers: {
-        Accept: 'application/json',
-        Authorization: 'Bearer {{bundle.authData.access_token}}',
-      },
-      url: 'https://api.atlassian.com/oauth/token/accessible-resources',
-    },
+    perform: siteOutputFields,
     sample: {
       id: 'c74b7111-8d83-4833-8b36-dcebb57fae89',
       url: 'https://agileops.atlassian.net',
@@ -17,7 +46,7 @@ module.exports = {
     },
     outputFields: [
       { key: 'id', label: 'id', type: 'string' },
-      { key: 'url', label: 'url', type: 'string' },
+      { key: 'url', label: 'url', type: 'string' }
     ],
   },
   key: 'fetch_site_id',
