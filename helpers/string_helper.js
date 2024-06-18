4b5c94dd5ea84244d2eca02f15a872945ab208c2
Hoang An <hoangansu9@gmail.com>
2024-06-18 15:59:44 +0700

    Add throw error attachment name

--- helpers/string_helper.js
+++ helpers/string_helper.js
@@ -6,7 +6,14 @@ function checkHyperlinks(input) {
     return new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(input)
 }
 
-module.exports = {
-    stripHtmlTags: stripHtmlTags,
-    checkHyperlinks: checkHyperlinks
+function checkIsVietnameseCharacter(str) {
+	const regex = /^[a-zA-Z0-9\s_.-]+$/;
+	const isNormalTone = regex.test(str);
+	return !isNormalTone;
 }
+
+module.exports = {
+	stripHtmlTags: stripHtmlTags,
+	checkHyperlinks: checkHyperlinks,
+	checkIsVietnameseCharacter: checkIsVietnameseCharacter,
+};
