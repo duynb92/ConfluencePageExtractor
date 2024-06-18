ca1f88a1aabb1ee3bae3fe87943ee72a923df89a
Hoang An <hoangansu9@gmail.com>
2024-06-18 16:01:47 +0700

    Add throw error attachment name

--- creates/extract_issue_content.js
+++ creates/extract_issue_content.js
@@ -57,7 +57,6 @@ const perform = async (z, bundle) => {
                         }
                     }),
                     attachments: res.json.fields['attachment'].map(x => {
-                        z.console.log(x.filename);
                         return {
                             id: x.id,
                             name: x.filename,
